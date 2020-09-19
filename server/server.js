8db04742b5f5e720e940877ecab8f640fb8bc421
Mateusz Kopij <sagasu@gmail.com>
2020-09-19 22:15:44 +0100

    introducing even loop communication between server and client.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const io = require('socket.io')();
+const {createGameState} = require('./game');
+const {FRAME_RATE} = require('./constants');
 
 io.on('connection', client => {
     client.emit('init', {data: 'hello world'});
+    createGameInterval(client, state);
 });
+
+function createGameInterval(client, state) {
+    const intervalId = setInterval(() => {
+        const winner = gameLoop(state);
+    }, 1000 /FRAME_RATE);
+}
 
 io.listen(3000);
